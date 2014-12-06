288e76d49a21b7ec4ff73b6f32df623bef5dc0a3
Václav Legát <legatva1@fel.cvut.cz>
2014-12-06 14:48:54 +0100

    Initial commit

---
+++ src/main/java/cz/sep/app/App.java
@@ -0,0 +1,28 @@
+package cz.sep.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public int plus(int a, int b) {
+        return a+b;
+    }
+
+    public int minus(int a, int b) {
+        return a-b;
+    }
+
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/test/java/cz/sep/app/AppTest.java
@@ -0,0 +1,57 @@
+package cz.sep.app;
+
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+    App app;
+
+
+    @BeforeClass
+    public void setUp() throws Exception {
+
+        app=new App();
+    }
+
+    @Test
+    public void smokeTest() throws Exception{
+        Assert.assertEquals(app.plus(8, 6), 14);
+        Assert.assertEquals(app.minus(8, 6), 2);
+        Assert.assertEquals(app.multiply(8, 6), 48);
+    }
+
+
+    @Test
+    public void testPlus() throws Exception {
+
+        Assert.assertEquals(app.plus(8, 6), 14);
+        Assert.assertEquals(app.plus(8, -6), 2);
+        Assert.assertEquals(app.plus(-8, 6), -2);
+        Assert.assertEquals(app.plus(-8, -6), -14);
+
+    }
+
+    @Test
+    public void testMinus() throws Exception {
+
+        Assert.assertEquals(app.minus(8, 6), 2);
+        Assert.assertEquals(app.minus(8, -6), 14);
+        Assert.assertEquals(app.minus(-8, 6), -14);
+        Assert.assertEquals(app.minus(-8, -6), -2);
+
+    }
+
+    @Test
+    public void multiply() throws Exception {
+
+        Assert.assertEquals(app.multiply(8, 6), 48);
+        Assert.assertEquals(app.multiply(8, -6), -48);
+        Assert.assertEquals(app.multiply(-8, 6), -48);
+        Assert.assertEquals(app.multiply(-8, -6), 48);
+        Assert.assertEquals(app.multiply(9, 0), 0);
+
+    }
+}
